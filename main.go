de006c4a39ba82e59f585f260a46f4f4affa3171
Chanwit Piromplad <kingkong2103@gmail.com>
2018-06-09 09:44:52 +0700

    pass by ref

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ type MyType struct {
 }
 
 func main() {
-	myvalue := MyType{Value1: 1, Value2: "ss"}
-	pointer := unsafe.Pointer(&myvalue)
+	myvalue := &MyType{Value1: 1, Value2: "ss"}
+	pointer := unsafe.Pointer(myvalue)
 	fmt.Printf("Addr: %v value1: %d value2: %s\n", pointer, myvalue.Value1, myvalue.Value2)
 
 	changeMyValue(myvalue)
 	fmt.Printf("Addr: %v value1: %d value2: %s\n", pointer, myvalue.Value1, myvalue.Value2)
 }
 
-func changeMyValue(myvalue MyType) {
+func changeMyValue(myvalue *MyType) {
 	myvalue.Value1 = 20
 	myvalue.Value2 = "ton"
 
-	pointer := unsafe.Pointer(&myvalue)
+	pointer := unsafe.Pointer(myvalue)
 	fmt.Printf("Addr: %v value1: %d value2: %s\n", pointer, myvalue.Value1, myvalue.Value2)
 
 }
